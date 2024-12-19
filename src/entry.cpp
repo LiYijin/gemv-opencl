b322a7c68365594c8091a47ed38d1d92ee739b60
liyijin <liyijin@xcoresigma.com>
2024-12-19 18:24:29 +0800

    fix layout

--- src/entry.cpp
+++ src/entry.cpp
@@ -16,6 +16,15 @@ static const std::string stringifiedKernels =
 #include "gemm.cl"
     ;
 
+#define CL_CHECK(err)                                               \
+    do {                                                            \
+        cl_int err_ = (err);                                        \
+        if (err_ != CL_SUCCESS) {                                   \
+            fprintf(stderr, "ggml_opencl: %s error %d at %s:%d\n",  \
+                #err, err_, __FILE__, __LINE__);                    \
+            exit(1);                                                \
+        }                                                           \
+    } while (0)
 
 double run_kernel(cl::CommandQueue &queue, cl::Kernel &kernel, cl::NDRange &globalSize, cl::NDRange &localSize, uint iters)
 {
@@ -38,6 +47,7 @@ double run_kernel(cl::CommandQueue &queue, cl::Kernel &kernel, cl::NDRange &glob
   return (timed / static_cast<float>(iters));
 }
 
+// layout: |1|0| |3|2| ...
 void gemv_int4_fp32(const uint8_t* A, const float* B, const float* scale, const float* bias, float* C, const int m, const int n, const int group_size) {
   const int scale_per_row = n / group_size;
   for (int row = 0; row < m; row++) {
@@ -76,23 +86,44 @@ void matrix_transpose(const float * ori, float * after, int m, int n, bool flag 
   }
 }
 
+// change |1|0|  |3|2|  to |16|0| |17|1|
+void matrix_mix(uint8_t * ori, uint8_t * after, int m, int n, int group_size) {
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j += group_size) {
+        uint8_t* after_off = after + i * n / 2 + j / 2;
+        uint8_t* ori_off = ori + i * n / 2 + j / 2;
+        for (int k = 0; k < group_size / 4; k += 1) {
+          after_off[2 * k] = (0xf0 & (ori_off[k + group_size / 4] << 4)) | (0xf & ori_off[k]);
+          after_off[2 * k + 1] = (0xf0 & (ori_off[k + group_size / 4])) | (0xf & (ori_off[k] >> 4));
+        }
+      }
+    }
+    printf("\n");
+    for (int i = 0; i < 16;i++) {
+      printf(" %x(%d), ", ori[i], i);
+    }
+    printf("\n");
+    for (int i = 0; i < 16;i++) {
+      printf(" %x(%d), ", after[i],i);
+    }    
+}
 
 void initializeData(uint8_t* A, float* B, float* scale, float* C, float* bias, const int m, const int n, const int group_size) {
   for (int i = 0; i < m * n / 2; i++) {
-    A[i] = 1;//rand() % 256;
+    A[i] = rand() % 256;
   }
   for (int i = 0; i < n; i++) {
-    B[i] = 1;//rand() % 1024;
+    B[i] = rand() % 1024;
 
   }
   for (int i = 0; i < n; i++) {
     C[i] = 1;
   }
   for (int i = 0; i < n; i++) {
-    bias[i] = 1;//rand() % 1024;
+    bias[i] = rand() % 1024;
   }
   for (int i = 0; i < m * n / group_size; i++) {
-    scale[i] = 1;//rand() % 10;
+    scale[i] = rand() % 10;
   }
 }
 
@@ -110,6 +141,19 @@ void row_major2column_major(T* A, const int m, const int k) {
   free(Ac);
 }
 
+void row_major2column_major_block(uint8_t* A, const int m, const int k, const int block_byte) {
+  uint8_t* Ac = (uint8_t*)malloc(m * k * block_byte);
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < k; j++) {
+      int src_idx = i * k + j;
+      int dst_idx = j * m + k;
+      memcpy(Ac + dst_idx * block_byte, A + src_idx * block_byte, block_byte);
+    }
+  }
+  memcpy(A, Ac, m * k * block_byte);
+  free(Ac);
+}
+
 int main(int argc, char **argv)
 {
     // gemv, A[m, n], B[n,1], C[m,1]
@@ -118,7 +162,7 @@ int main(int argc, char **argv)
     const int m = 4096;
     // const int k = 4096;
     const int n = 4096;
-    const int group_size = 128;
+    const int group_size = 32;
     const int iters = 1;
     uint8_t* A = (uint8_t*)malloc(m * n * sizeof(uint8_t) / 2);
     float* scale = (float*)malloc(m * n / group_size * sizeof(float));
@@ -204,20 +248,68 @@ int main(int argc, char **argv)
 
             cl::NDRange globalSize, localSize;
 
-            cl::Kernel kernel_gemm(prog, "gemv_int4_fp32");
+            gemv_int4_fp32(A, B, scale, bias, C_expect, m, n, group_size);
 
             // gemv_int4_fp32 param
+            // {
+            //   cl::Kernel kernel_gemm(prog, "gemv_int4_fp32");
+            //   globalSize = {m}, localSize = {64};
+            //   kernel_gemm.setArg(0, ABuf), kernel_gemm.setArg(1, BBuf);
+            //   kernel_gemm.setArg(2, scaleBuf), kernel_gemm.setArg(3, biasBuf);
+            //   kernel_gemm.setArg(4, CBuf);
+            //   kernel_gemm.setArg(5, m), kernel_gemm.setArg(6, n);
+            //   kernel_gemm.setArg(7, group_size);
+            //   timed = run_kernel(queue, kernel_gemm, globalSize, localSize, iters);
+
+            //   queue.finish();
+            //   queue.enqueueReadBuffer(CBuf, CL_TRUE, 0, m * sizeof(float), C);
+            //   queue.finish();
+
+            //   // matrix_transpose(C, C_T, n, m, false);
+            // }
+            // llama.cpp kernel _v2
+            // {
+            //   uint8_t* A_mix = (uint8_t*)malloc(m * n * sizeof(uint8_t) / 2);
+            //   matrix_mix(A, A_mix, m, n, group_size);
+            //   queue.enqueueWriteBuffer(ABuf, CL_TRUE, 0, m * n / 2, A_mix);
+            //   cl::Kernel kernel_gemm(prog, "gemv_int4_fp32_v2");
+            //   const int local_size = 64;
+            //   globalSize = {m * 64}, localSize = {64};
+
+            //   cl::Buffer tmp = cl::Buffer(ctx, CL_MEM_READ_WRITE, local_size);
+
+            //   kernel_gemm.setArg(0, ABuf), kernel_gemm.setArg(1, BBuf);
+            //   kernel_gemm.setArg(2, scaleBuf), kernel_gemm.setArg(3, biasBuf);
+            //   kernel_gemm.setArg(4, CBuf);
+            //   kernel_gemm.setArg(5, m), kernel_gemm.setArg(6, n);
+            //   kernel_gemm.setArg(7, group_size);
+            //   kernel_gemm.setArg(8, tmp);
+            //   timed = run_kernel(queue, kernel_gemm, globalSize, localSize, iters);
+
+            //   queue.finish();
+            //   queue.enqueueReadBuffer(CBuf, CL_TRUE, 0, m * sizeof(float), C);
+            //   queue.finish();
+            //   free(A_mix);
+            // }
+
             {
-              gemv_int4_fp32(A, B, scale, bias, C_expect, m, n, group_size);
+              uint8_t* A_mix = (uint8_t*)malloc(m * n * sizeof(uint8_t) / 2);
+              matrix_mix(A, A_mix, m, n, group_size);
+              // row_major2column_major_block(A_mix, m, n / group_size, group_size / 2);
+              queue.enqueueWriteBuffer(ABuf, CL_TRUE, 0, m * n / 2, A_mix);
+              free(A_mix);
+              cl::Kernel kernel_gemm(prog, "gemv_int4_fp32_v3");
+              const int local_size = 64;
+              globalSize = {m}, localSize = {local_size};
 
+              cl::Buffer tmp = cl::Buffer(ctx, CL_MEM_READ_WRITE, local_size);
 
-
-              globalSize = {m}, localSize = {64};
               kernel_gemm.setArg(0, ABuf), kernel_gemm.setArg(1, BBuf);
               kernel_gemm.setArg(2, scaleBuf), kernel_gemm.setArg(3, biasBuf);
               kernel_gemm.setArg(4, CBuf);
               kernel_gemm.setArg(5, m), kernel_gemm.setArg(6, n);
               kernel_gemm.setArg(7, group_size);
+              kernel_gemm.setArg(8, tmp);
               timed = run_kernel(queue, kernel_gemm, globalSize, localSize, iters);
 
               queue.finish();
@@ -248,7 +340,7 @@ int main(int argc, char **argv)
             PRINT("us, compute is : ");
             PRINT(gflops);
             PRINT(" GFlops, bandwidth is : ");
-            PRINT(gB);
+            PRINT(gB / timed * 1e6);
             PRINT(" GB/s");
             PRINT(NEWLINE);
         }
--- src/kernel/gemm.cl
+++ src/kernel/gemm.cl
@@ -1,4 +1,6 @@
 R"(
+typedef char int8_t;
+typedef uchar uint8_t;
 
 // A layout: |1|0|   |3|2|   |..|
 // every thread process one row.
@@ -25,5 +27,96 @@ __kernel void gemv_int4_fp32(__global char *A, __global float *B, __global float
     // Store the result
     C[row] = acc + bias[row]; 
 }
+// every group process one row.
+// work-item in on work-group process adjacent 
+// weight layout: |16|0|  |17|1| .. 
+__kernel void gemv_int4_fp32_v2(__global char *A, __global float *B, __global float *scale, __global float* bias, __global float* C, const int M, const int N, const int group_size, __local float* tmp)
+{
+  const int local_size = get_local_size(0);
+  const int row = get_group_id(0);
+  const int tid = get_local_id(0);
+  const int col_step = local_size * 2;
+  const int y_offset = group_size / 2;
+
+  tmp[tid] = 0;
+  for (int col = tid * 2; col < N; col += col_step) {
+      const int ib = (row * N + col) / group_size; // block index
+      const int iybs = col - col % group_size; // y block start index
+      const int iqs = (col % group_size) / 2; // quant index
+
+      // dequantize
+      float v0, v1;
+
+      // load scale value.
+      const float sc = scale[ib];
+
+      const uint8_t vui = A[(row * N + col) / 2];
+
+      v0 = ((vui & 0xF) - 8) * sc;
+      v1 = (((vui >> 4) & 0xF) - 8) * sc;
+
+      // matrix multiplication
+      tmp[tid] += v0 * B[iybs + iqs];
+      tmp[tid] += v1 * B[iybs + iqs + y_offset];
+  }
+
+  // sum up partial sums and write back result
+  barrier(CLK_LOCAL_MEM_FENCE);
+  for (int s = local_size / 2; s > 0; s >>= 1) {
+      if (tid < s) {
+          tmp[tid] += tmp[tid + s];
+      }
+      barrier(CLK_LOCAL_MEM_FENCE);
+  }
+  if (tid == 0) {
+      C[row] = tmp[0] + bias[row];
+  }
+}
+
+// 
+// every work-item process one row.
+// |16|0|  |17|1| .. 
+__kernel void gemv_int4_fp32_v3(__global char *A, __global float4 *B, __global float *scale, __global float* bias, __global float* C, const int M, const int N, const int group_size, __local float* tmp)
+{
+    const int local_size = get_local_size(0);
+    const int row = get_global_id(0);
+    const int col_step = group_size;
+    float acc = 0.0f;
+    for (int col = 0; col < N; col += col_step) {
+        const int iybs = col / 4;
+
+        float4 y00 = B[iybs + 0];
+        float4 y10 = B[iybs + 4];
+        float4 y01 = B[iybs + 1];
+        float4 y11 = B[iybs + 5];
+        float4 y02 = B[iybs + 2];
+        float4 y12 = B[iybs + 6];
+        float4 y03 = B[iybs + 3];
+        float4 y13 = B[iybs + 7];
+
+        // const int ib = row +  col / group_size * get_num_groups(0) * get_local_size(0);
+        const int ib = row * N / group_size +  col / group_size;
+        const float sc = scale[ib];
+        const int4 vui = *((__global int4*)(A) + ib);
+        const float4 v00 = convert_float4((char)-8 + as_char4(vui.x & 0x0f0f0f0f));
+        const float4 v10 = convert_float4((char)-8 + as_char4((vui.x >> 4) & 0x0f0f0f0f));
+        const float4 v01 = convert_float4((char)-8 + as_char4(vui.y & 0x0f0f0f0f));
+        const float4 v11 = convert_float4((char)-8 + as_char4((vui.y >> 4) & 0x0f0f0f0f));
+        const float4 v02 = convert_float4((char)-8 + as_char4(vui.z & 0x0f0f0f0f));
+        const float4 v12 = convert_float4((char)-8 + as_char4((vui.z >> 4) & 0x0f0f0f0f));                
+        const float4 v03 = convert_float4((char)-8 + as_char4(vui.w & 0x0f0f0f0f));
+        const float4 v13 = convert_float4((char)-8 + as_char4((vui.w >> 4) & 0x0f0f0f0f));
+        acc += sc * dot(v00, y00);
+        acc += sc * dot(v10, y10);
+        acc += sc * dot(v01, y01);
+        acc += sc * dot(v11, y11);
+        acc += sc * dot(v02, y02);
+        acc += sc * dot(v12, y12);
+        acc += sc * dot(v03, y03);
+        acc += sc * dot(v13, y13);
+    }
+    C[row] = acc + bias[row]; 
+}
+
 
 )"
